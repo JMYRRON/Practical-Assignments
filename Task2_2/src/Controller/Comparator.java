961854d1b566178f708e2257daada1f478b5d10e
Ivan Myronenko <j.myrron@gmail.com>
2019-12-11 14:39:51 +0200

    Add files via upload

---
+++ Task2_2/src/Controller/Comparator.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import Model.Book;
+
+public class Comparator implements java.util.Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        Book book1 = (Book)o1;
+        Book book2 = (Book)o2;
+
+
+        return book1.getPublisher().compareTo(book2.getPublisher());
+    }
+}
---
+++ Task2_2/src/Controller/Menu.java
@@ -0,0 +1,88 @@
+package Controller;
+
+import Model.Book;
+
+import java.util.Arrays;
+
+public class Menu {
+
+    private Book[] books;
+
+    public Menu(Book[] books) {
+        this.books = books;
+    }
+
+    public void getBooksByAuthor (String author) {
+        int authorBooksNumber = 0;
+        for (Book book:books) {
+            if (book.getAuthor().equals(author)) {
+                authorBooksNumber++;
+            }
+        }
+        Book[] authorbooks = new Book[authorBooksNumber];
+        int index = 0;
+        for (Book book:books) {
+            if (book.getAuthor().equals(author)) {
+                authorbooks[index] = book;
+                index++;
+            }
+        }
+
+        printBooks(authorbooks);
+
+    }
+
+    public void getByPublisher (String publisher) {
+
+        int publisherBooksNumber = 0;
+        for (Book book:books) {
+            if (book.getPublisher().equals(publisher)) {
+                publisherBooksNumber++;
+            }
+        }
+        Book[] publisherbooks = new Book[publisherBooksNumber];
+        int index = 0;
+        for (Book book:books) {
+            if (book.getPublisher().equals(publisher)) {
+                publisherbooks[index] = book;
+                index++;
+            }
+        }
+
+        printBooks(publisherbooks);
+    }
+
+    public void getBooksByYear (int year) {
+
+        int yearBooksNumber = 0;
+        for (Book book:books) {
+            if (book.getYear() == year) {
+                yearBooksNumber++;
+            }
+        }
+        Book[] yearbooks = new Book[yearBooksNumber];
+        int index = 0;
+        for (Book book:books) {
+            if (book.getYear() == year) {
+                yearbooks[index] = book;
+                index++;
+            }
+        }
+
+        printBooks(yearbooks);
+    }
+
+    public void sortByPublisher() {
+
+        Arrays.sort(books, new Comparator());
+        printBooks(books);
+
+    }
+
+    private static void printBooks(Book[] books) {
+        for (Book book:books) {
+            System.out.println(book);
+        }
+    }
+
+}
